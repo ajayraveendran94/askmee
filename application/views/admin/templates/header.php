31342be7e30fb9f7e36a5e689c4e16b2b50d7d04
unknown <ajayraveendran94@gmail.com>
2021-03-27 02:53:47 +0530

    Initial Commit

---
+++ application/views/admin/templates/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- auth-login.html  21 Nov 2019 03:49:32 GMT -->
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Askmee - Admin</title>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/app.min.css");?>"> 
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css");?>"> 
+  <!-- app.min.css -->
+  <link rel="stylesheet" href="">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="<?php echo base_url("assets/bundles/bootstrap-social/bootstrap-social.css"); ?>">
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/components.css"); ?>">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/custom.css"); ?>">
+  <link rel="stylesheet" href="<?php echo base_url("assets/img/favicon.ico"); ?>">
+</head>
