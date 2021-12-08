724602d4f4908c2ee1462f2de813a0a624bdbc73
ajayraveendran94 <ajayraveendran94@gmail.com>
2021-12-08 22:34:57 +0530

    DB changes

--- askme_ecom.sql
+++ askme_ecom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 22, 2021 at 09:22 AM
+-- Generation Time: Dec 08, 2021 at 06:03 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -104,6 +104,27 @@ INSERT INTO `as_checkout` (`ch_id`, `ch_pr_id`, `ch_user_id`, `ch_quantity`) VAL
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `as_commission`
+--
+
+CREATE TABLE `as_commission` (
+  `com_id` bigint(20) UNSIGNED NOT NULL,
+  `com_name` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `com_amount` float DEFAULT NULL,
+  `com_percent` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `as_commission`
+--
+
+INSERT INTO `as_commission` (`com_id`, `com_name`, `com_amount`, `com_percent`) VALUES
+(1, 'General', NULL, 5),
+(2, 'Minimum Amount', 35, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `as_orders`
 --
 
@@ -111,7 +132,6 @@ CREATE TABLE `as_orders` (
   `or_id` bigint(20) NOT NULL,
   `address_id` bigint(20) NOT NULL,
   `total_amount` float NOT NULL,
-  `status_id` bigint(20) NOT NULL,
   `order_from_admin` tinyint(1) NOT NULL DEFAULT 0,
   `order_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -120,19 +140,19 @@ CREATE TABLE `as_orders` (
 -- Dumping data for table `as_orders`
 --
 
-INSERT INTO `as_orders` (`or_id`, `address_id`, `total_amount`, `status_id`, `order_from_admin`, `order_date`) VALUES
-(1, 1, 828, 1, 1, '2021-07-18 13:29:44'),
-(9, 3, 194361, 1, 0, '2021-09-05 22:09:48'),
-(11, 5, 676, 1, 0, '2021-09-05 22:26:31'),
-(12, 2, 224, 1, 0, '2021-09-17 21:00:37'),
-(14, 2, 254, 1, 0, '2021-09-17 21:03:41'),
-(16, 3, 254, 1, 0, '2021-09-17 21:10:21'),
-(18, 2, 1042, 1, 0, '2021-10-31 09:46:56'),
-(20, 2, 696, 1, 0, '2021-11-20 12:53:58'),
-(21, 2, 56, 1, 0, '2021-11-20 12:54:19'),
-(22, 2, 56, 1, 0, '2021-11-20 19:06:10'),
-(23, 8, 56, 1, 0, '2021-11-20 19:22:53'),
-(24, 9, 129940, 1, 0, '2021-11-20 19:26:17');
+INSERT INTO `as_orders` (`or_id`, `address_id`, `total_amount`, `order_from_admin`, `order_date`) VALUES
+(1, 1, 828, 1, '2021-07-18 13:29:44'),
+(9, 3, 194361, 0, '2021-09-05 22:09:48'),
+(11, 5, 676, 0, '2021-09-05 22:26:31'),
+(12, 2, 224, 0, '2021-09-17 21:00:37'),
+(14, 2, 254, 0, '2021-09-17 21:03:41'),
+(16, 3, 254, 0, '2021-09-17 21:10:21'),
+(18, 2, 1042, 0, '2021-10-31 09:46:56'),
+(20, 2, 696, 0, '2021-11-20 12:53:58'),
+(21, 2, 56, 0, '2021-11-20 12:54:19'),
+(22, 2, 56, 0, '2021-11-20 19:06:10'),
+(23, 8, 56, 0, '2021-11-20 19:22:53'),
+(24, 9, 129940, 0, '2021-11-20 19:26:17');
 
 -- --------------------------------------------------------
 
@@ -147,32 +167,33 @@ CREATE TABLE `as_order_detail` (
   `or_quantity` bigint(10) NOT NULL,
   `individual_price` float NOT NULL,
   `total_price` float NOT NULL,
-  `delivery_date` date NOT NULL
+  `delivery_date` date NOT NULL,
+  `status_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `as_order_detail`
 --
 
-INSERT INTO `as_order_detail` (`or_detail_id`, `order_id`, `or_product_id`, `or_quantity`, `individual_price`, `total_price`, `delivery_date`) VALUES
-(1, 1, 1, 2, 254, 508, '0000-00-00'),
-(2, 1, 3, 1, 320, 320, '0000-00-00'),
-(10, 9, 4, 3, 64787, 194361, '0000-00-00'),
-(12, 11, 5, 3, 56, 168, '0000-00-00'),
-(13, 11, 1, 2, 254, 508, '0000-00-00'),
-(14, 12, 5, 4, 56, 224, '0000-00-00'),
-(16, 14, 1, 1, 254, 254, '0000-00-00'),
-(18, 16, 1, 1, 254, 254, '0000-00-00'),
-(20, 18, 5, 5, 56, 280, '0000-00-00'),
-(21, 18, 1, 3, 254, 762, '0000-00-00'),
-(24, 20, 3, 2, 320, 640, '0000-00-00'),
-(25, 20, 5, 1, 56, 56, '0000-00-00'),
-(26, 21, 5, 1, 56, 56, '0000-00-00'),
-(27, 22, 5, 1, 56, 56, '0000-00-00'),
-(28, 23, 5, 1, 56, 56, '0000-00-00'),
-(29, 24, 1, 1, 254, 254, '0000-00-00'),
-(30, 24, 4, 2, 64787, 129574, '0000-00-00'),
-(31, 24, 5, 2, 56, 112, '0000-00-00');
+INSERT INTO `as_order_detail` (`or_detail_id`, `order_id`, `or_product_id`, `or_quantity`, `individual_price`, `total_price`, `delivery_date`, `status_id`) VALUES
+(1, 1, 1, 2, 254, 508, '2021-12-26', 1),
+(2, 1, 3, 1, 320, 320, '0000-00-00', 1),
+(10, 9, 4, 3, 64787, 194361, '0000-00-00', 1),
+(12, 11, 5, 3, 56, 168, '0000-00-00', 1),
+(13, 11, 1, 2, 254, 508, '0000-00-00', 1),
+(14, 12, 5, 4, 56, 224, '0000-00-00', 1),
+(16, 14, 1, 1, 254, 254, '0000-00-00', 1),
+(18, 16, 1, 1, 254, 254, '0000-00-00', 1),
+(20, 18, 5, 5, 56, 280, '0000-00-00', 1),
+(21, 18, 1, 3, 254, 762, '0000-00-00', 1),
+(24, 20, 3, 2, 320, 640, '0000-00-00', 1),
+(25, 20, 5, 1, 56, 56, '0000-00-00', 1),
+(26, 21, 5, 1, 56, 56, '0000-00-00', 1),
+(27, 22, 5, 1, 56, 56, '0000-00-00', 1),
+(28, 23, 5, 1, 56, 56, '0000-00-00', 1),
+(29, 24, 1, 1, 254, 254, '0000-00-00', 1),
+(30, 24, 4, 2, 64787, 129574, '0000-00-00', 1),
+(31, 24, 5, 2, 56, 112, '0000-00-00', 1);
 
 -- --------------------------------------------------------
 
@@ -205,6 +226,7 @@ CREATE TABLE `as_products` (
   `master_product_id` bigint(20) NOT NULL,
   `vendor_id` bigint(20) UNSIGNED NOT NULL,
   `actual_price` float NOT NULL,
+  `vendor_price` float NOT NULL,
   `offer_price` float NOT NULL,
   `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `quantity` bigint(10) NOT NULL,
@@ -215,11 +237,15 @@ CREATE TABLE `as_products` (
 -- Dumping data for table `as_products`
 --
 
-INSERT INTO `as_products` (`p_id`, `master_product_id`, `vendor_id`, `actual_price`, `offer_price`, `description`, `quantity`, `product_status`) VALUES
-(1, 1, 3, 304, 254, 'The green chromide is a species of cichlid fish that is native to fresh and brackish water habitats in some parts in India such as Kerala, Goa, Chilika Lake in Odisha and Sri Lanka. The species was first described by Marcus Elieser Bloch in 1790', 11, 1),
-(3, 1, 12, 360, 320, 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Let', 51, 1),
-(4, 2, 10, 74567, 64787, 'ThinkPad X1 Carbon - ThinkPads are unmistakable. While other OEMs focus on achieving the thinnest and lightest chassis, the perfect shade of rose gold to attract the masses, and nearly invisible bezels, Lenovo has given the ThinkPad family thoughtful updates that keep its original focus and also attempt to stay on top of the newest laptop design and use innovations.', 11, 1),
-(5, 5, 6, 78, 56, '1KG Red apple raw fruit backgrounds, natural healthy organic fresh product', 23, 1);
+INSERT INTO `as_products` (`p_id`, `master_product_id`, `vendor_id`, `actual_price`, `vendor_price`, `offer_price`, `description`, `quantity`, `product_status`) VALUES
+(1, 1, 3, 304, 0, 254, 'The green chromide is a species of cichlid fish that is native to fresh and brackish water habitats in some parts in India such as Kerala, Goa, Chilika Lake in Odisha and Sri Lanka. The species was first described by Marcus Elieser Bloch in 1790', 11, 1),
+(3, 1, 12, 360, 0, 320, 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Let', 51, 1),
+(4, 2, 10, 74567, 0, 64787, 'ThinkPad X1 Carbon - ThinkPads are unmistakable. While other OEMs focus on achieving the thinnest and lightest chassis, the perfect shade of rose gold to attract the masses, and nearly invisible bezels, Lenovo has given the ThinkPad family thoughtful updates that keep its original focus and also attempt to stay on top of the newest laptop design and use innovations.', 11, 1),
+(5, 5, 6, 78, 0, 56, '1KG Red apple raw fruit backgrounds, natural healthy organic fresh product', 23, 1),
+(6, 6, 10, 38, 0, 21, 'Shop from a wide range of t-shirt from orianz. Pefect for your everyday use, you could pair it with a stylish pair of jeans or trousers complete the look.', 40, 1),
+(7, 7, 10, 190, 0, 165, 'Shop from a wide range of t-shirt from orianz. Pefect for your everyday use, you could pair it with a stylish pair of jeans or trousers complete the look.', 23, 1),
+(10, 13, 10, 50, 30, 65, 'lo', 42, 1),
+(11, 10, 10, 200, 175, 210, 'Descer', 20, 1);
 
 -- --------------------------------------------------------
 
@@ -248,7 +274,12 @@ INSERT INTO `as_product_images` (`id`, `product_id`, `image_url`) VALUES
 (17, 4, 'Img_Lap1.jpg'),
 (18, 5, 'apple_2.jpg'),
 (19, 5, 'apple_1.jpg'),
-(20, 5, 'apple.jpg');
+(20, 5, 'apple.jpg'),
+(21, 6, 'carrot_1.png'),
+(22, 6, 'carrot_1.png'),
+(23, 7, 'Kalanji.png'),
+(26, 10, 'carrot_1.png'),
+(27, 11, 'carrot_1.png');
 
 -- --------------------------------------------------------
 
@@ -259,17 +290,23 @@ INSERT INTO `as_product_images` (`id`, `product_id`, `image_url`) VALUES
 CREATE TABLE `as_product_master` (
   `id` bigint(20) NOT NULL,
   `product_name` varchar(90) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `category_id` bigint(20) NOT NULL
+  `category_id` bigint(20) NOT NULL,
+  `commission_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `as_product_master`
 --
 
-INSERT INTO `as_product_master` (`id`, `product_name`, `category_id`) VALUES
-(1, 'കരിമീൻ ', 3),
-(2, 'Laptop', 5),
-(5, 'Apple', 6);
+INSERT INTO `as_product_master` (`id`, `product_name`, `category_id`, `commission_id`) VALUES
+(1, 'കരിമീൻ ', 3, 1),
+(2, 'Laptop', 5, 1),
+(5, 'Apple', 6, 1),
+(6, 'Carrot (1Kg)', 6, 1),
+(7, 'Kalanji (1 Kg)', 3, 1),
+(8, 'Realme A30', 5, 1),
+(10, 'Orange (2KG)', 6, 2),
+(13, 'Carrot (1 KG)', 6, 2);
 
 -- --------------------------------------------------------
 
@@ -318,6 +355,38 @@ CREATE TABLE `as_user_cart` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `as_user_cart`
+--
+
+INSERT INTO `as_user_cart` (`car_id`, `car_pr_id`, `car_user_id`, `car_quantity`) VALUES
+(75, 3, 2, 5),
+(76, 7, 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `as_user_reviews`
+--
+
+CREATE TABLE `as_user_reviews` (
+  `ur_id` bigint(50) NOT NULL,
+  `ur_review` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
+  `ur_rating` bigint(1) NOT NULL,
+  `ur_user_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `as_user_review_images`
+--
+
+CREATE TABLE `as_user_review_images` (
+  `img_id` bigint(25) NOT NULL,
+  `review_id` bigint(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
 -- Indexes for dumped tables
 --
 
@@ -342,12 +411,17 @@ ALTER TABLE `as_checkout`
   ADD PRIMARY KEY (`ch_id`);
 
 --
+-- Indexes for table `as_commission`
+--
+ALTER TABLE `as_commission`
+  ADD PRIMARY KEY (`com_id`);
+
+--
 -- Indexes for table `as_orders`
 --
 ALTER TABLE `as_orders`
   ADD PRIMARY KEY (`or_id`),
-  ADD KEY `address_to_order` (`address_id`),
-  ADD KEY `status_to_order` (`status_id`);
+  ADD KEY `address_to_order` (`address_id`);
 
 --
 -- Indexes for table `as_order_detail`
@@ -356,7 +430,8 @@ ALTER TABLE `as_order_detail`
   ADD PRIMARY KEY (`or_detail_id`),
   ADD UNIQUE KEY `or_detail_id` (`or_detail_id`),
   ADD KEY `order_in_detail` (`order_id`),
-  ADD KEY `product_in_order` (`or_product_id`);
+  ADD KEY `product_in_order` (`or_product_id`),
+  ADD KEY `status in order` (`status_id`);
 
 --
 -- Indexes for table `as_order_status`
@@ -384,7 +459,9 @@ ALTER TABLE `as_product_images`
 --
 ALTER TABLE `as_product_master`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `category_to_product` (`category_id`);
+  ADD UNIQUE KEY `product_name` (`product_name`),
+  ADD KEY `category_to_product` (`category_id`),
+  ADD KEY `commision to product` (`commission_id`);
 
 --
 -- Indexes for table `as_user`
@@ -400,6 +477,19 @@ ALTER TABLE `as_user_cart`
   ADD PRIMARY KEY (`car_id`),
   ADD KEY `user_to_cart` (`car_user_id`),
   ADD KEY `product_to_cart` (`car_pr_id`);
+
+--
+-- Indexes for table `as_user_reviews`
+--
+ALTER TABLE `as_user_reviews`
+  ADD PRIMARY KEY (`ur_id`),
+  ADD KEY `user_to_review` (`ur_user_id`);
+
+--
+-- Indexes for table `as_user_review_images`
+--
+ALTER TABLE `as_user_review_images`
+  ADD KEY `image_to_review` (`review_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -424,6 +514,12 @@ ALTER TABLE `as_checkout`
   MODIFY `ch_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
+-- AUTO_INCREMENT for table `as_commission`
+--
+ALTER TABLE `as_commission`
+  MODIFY `com_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `as_orders`
 --
 ALTER TABLE `as_orders`
@@ -445,19 +541,19 @@ ALTER TABLE `as_order_status`
 -- AUTO_INCREMENT for table `as_products`
 --
 ALTER TABLE `as_products`
-  MODIFY `p_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `p_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `as_product_images`
 --
 ALTER TABLE `as_product_images`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `as_product_master`
 --
 ALTER TABLE `as_product_master`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `as_user`
@@ -469,7 +565,13 @@ ALTER TABLE `as_user`
 -- AUTO_INCREMENT for table `as_user_cart`
 --
 ALTER TABLE `as_user_cart`
-  MODIFY `car_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `car_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT for table `as_user_reviews`
+--
+ALTER TABLE `as_user_reviews`
+  MODIFY `ur_id` bigint(50) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -485,15 +587,15 @@ ALTER TABLE `as_address`
 -- Constraints for table `as_orders`
 --
 ALTER TABLE `as_orders`
-  ADD CONSTRAINT `address_to_order` FOREIGN KEY (`address_id`) REFERENCES `as_address` (`ad_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `status_to_order` FOREIGN KEY (`status_id`) REFERENCES `as_order_status` (`ors_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `address_to_order` FOREIGN KEY (`address_id`) REFERENCES `as_address` (`ad_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `as_order_detail`
 --
 ALTER TABLE `as_order_detail`
   ADD CONSTRAINT `order_in_detail` FOREIGN KEY (`order_id`) REFERENCES `as_orders` (`or_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `product_in_order` FOREIGN KEY (`or_product_id`) REFERENCES `as_products` (`p_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `product_in_order` FOREIGN KEY (`or_product_id`) REFERENCES `as_products` (`p_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `status in order` FOREIGN KEY (`status_id`) REFERENCES `as_order_status` (`ors_id`);
 
 --
 -- Constraints for table `as_products`
@@ -512,7 +614,8 @@ ALTER TABLE `as_product_images`
 -- Constraints for table `as_product_master`
 --
 ALTER TABLE `as_product_master`
-  ADD CONSTRAINT `category_to_product` FOREIGN KEY (`category_id`) REFERENCES `as_categories` (`c_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `category_to_product` FOREIGN KEY (`category_id`) REFERENCES `as_categories` (`c_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `commision to product` FOREIGN KEY (`commission_id`) REFERENCES `as_commission` (`com_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `as_user_cart`
@@ -520,6 +623,18 @@ ALTER TABLE `as_product_master`
 ALTER TABLE `as_user_cart`
   ADD CONSTRAINT `product_to_cart` FOREIGN KEY (`car_pr_id`) REFERENCES `as_products` (`p_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `user_to_cart` FOREIGN KEY (`car_user_id`) REFERENCES `as_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `as_user_reviews`
+--
+ALTER TABLE `as_user_reviews`
+  ADD CONSTRAINT `user_to_review` FOREIGN KEY (`ur_user_id`) REFERENCES `as_user` (`user_id`);
+
+--
+-- Constraints for table `as_user_review_images`
+--
+ALTER TABLE `as_user_review_images`
+  ADD CONSTRAINT `image_to_review` FOREIGN KEY (`review_id`) REFERENCES `as_user_reviews` (`ur_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
